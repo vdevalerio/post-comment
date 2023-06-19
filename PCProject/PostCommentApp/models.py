3aa285a626d62ace2f0f0c0eb20420378a871370
Marcos Valerio <marcos@umobi.com.br>
2023-06-18 23:20:11 -0300

    Add models

--- PCProject/PostCommentApp/models.py
+++ PCProject/PostCommentApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    def __str__(self):
+        return self.content
+
+    title = models.CharField(max_length=100, default='Title')
+    content = models.TextField(null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    def __str__(self):
+        return self.content
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
