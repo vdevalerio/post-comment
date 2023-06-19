e435de3b5888e03c6261341f40920139c4e1f2ed
Marcos Valerio <marcos@umobi.com.br>
2023-06-18 23:21:53 -0300

    Rename app to use camel case

--- PCProject/PostCommentApp/apps.py
+++ PCProject/PostCommentApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PostcommentappConfig(AppConfig):
+class PostCommentAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'PostCommentApp'
